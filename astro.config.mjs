e99fe7c91ebca1eee2b2e383b371bbe96d9261e3
Ismaël Lajaaiti <ismael.lajaaiti@gmail.com>
2024-09-08 19:34:46 +0200

    Test new remote

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,20 @@
 import { defineConfig } from 'astro/config';
 import remarkMath from "remark-math";
 import rehypeKatex from "rehype-katex";
-import rehypeMathJax from "rehype-mathjax";
+// import rehypeMathJax from "rehype-mathjax";
 import expressiveCode from "astro-expressive-code";
 import { pluginCollapsibleSections } from '@expressive-code/plugin-collapsible-sections';
 import icon from "astro-icon";
-
 import tailwind from "@astrojs/tailwind";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://astro-tutorial-2048.netlify.app",
+    // site: "https://astro-tutorial-2048.netlify.app",
     integrations: [expressiveCode({
         plugins: [pluginCollapsibleSections()]
-    }), icon(), tailwind()],
+    }), icon(), tailwind(), react()],
     markdown: {
         extendDefaultPlugins: true,
         remarkPlugins: [remarkMath],
